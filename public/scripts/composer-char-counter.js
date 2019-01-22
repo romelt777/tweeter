ba5df4c0ad1a940d457e01844797fd397db42157
Romel Tan <romel.tan777@gmail.com>
2019-01-22 18:21:34 +0000

    added counter on tweeter, becomes red on negative, counts length of input.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function() {
+  console.log("Im working again");
+});
+
+
+
+
+//setting variable for character counter,
+var char = 140;
+var count = document.getElementsByClassName("counter");
+count[0].innerHTML = char;
+
+
+//get elements by class returns an object so we must reference which.
+//listens to text box, if key is pressed then function is run.
+
+var listen1 = document.getElementsByName("text");
+listen1[0].addEventListener("keypress", testFunction);
+function testFunction() {
+  // console.log(this.value.length);
+  char = 139 - this.value.length;
+  // console.log("tst", charLength);
+  if (char < 0){
+    count[0].innerHTML = char;
+    document.getElementsByClassName("counter")[0].style.color = "red";
+    return char;
+  } else {
+    count[0].innerHTML = char;
+    return char;
+  }
+}
+
+
+//backspace updates late, doesnt start on 140 1 keystroke late.
