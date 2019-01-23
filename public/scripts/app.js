077af27e1143d735927741156309898985b6ce2f
Romel Tan <romel.tan777@gmail.com>
2019-01-23 19:26:03 +0000

    able to add multiple tweets from hardcoded objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,6 @@
 
 
 $(document).ready(function() {
-  console.log("Im working again");
 
 function createTweetElement(tweetInfo){
 
@@ -44,11 +43,66 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
-var $tweet = createTweetElement(tweetData);
+function renderTweets(tweets) {
+//this function cycles through entire tweet data array and feeds each one to the createTweetElement function. saved the result of each function call.
+  var result = "";
+  for(let i = 0; i < tweets.length; i++){
+    result += createTweetElement(tweets[i]);
+  }
+  return result;
+}
 
-// Test / driver code (temporary)
-console.log("tst", $tweet); // to see what it looks like
-$('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+//appending the tweet to the container.
+
+$('.tweets-container').append(renderTweets(data));
+
 
 });
